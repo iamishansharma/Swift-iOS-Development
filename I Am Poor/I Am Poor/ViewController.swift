017d8dbf01db5a315ba03b1215252b7885ad4d0c
Ishan Sharma <sharmaishan747@gmail.com>
2020-07-06 23:04:39 +0530

    I Am Poor

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Ishan Sharma on 06/07/20.
+//  Copyright © 2020 Ishan Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
