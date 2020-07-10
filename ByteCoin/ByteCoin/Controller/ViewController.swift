a1739d96ba068183053efa4a159e602924ed6d2f
Ishan Sharma <sharmaishan747@gmail.com>
2020-07-10 23:43:39 +0530

    10/07 ByteCoin

--- ByteCoin/ByteCoin/Controller/ViewController.swift
+++ ByteCoin/ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var pickerLabel: UIPickerView!
 
-    override func viewDidLoad() {
+    var coinManager = CoinManager();
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pickerLabel.dataSource = self;
+        pickerLabel.delegate = self;
+        coinManager.delegate = self;
     }
-
-
 }
 
+extension ViewController : UIPickerViewDataSource, UIPickerViewDelegate
+{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return coinManager.currencyArray.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return coinManager.currencyArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
+}
+
+extension ViewController : CoinManagerDelegate
+{
+    func didUpdatePrice(price: String, currency: String)
+    {
+        DispatchQueue.main.async
+        {
+            self.valueLabel.text = price;
+            self.currencyLabel.text = currency;
+        }
+    }
+
+    func didFailWithError(error: Error)
+    {
+        print(error);
+    }
+}
--- ByteCoin/ByteCoin/Model/CoinManager.swift
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,69 @@
 
 import Foundation
 
-struct CoinManager {
-    
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+protocol CoinManagerDelegate
+{
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
 
+struct CoinManager
+{
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC";
+    let apiKey = "F868F3C8-EADD-4B9C-89AE-FF14D4FFBE1E";
+
+    var delegate: CoinManagerDelegate?
+
+    // Eg URL : https://rest.coinapi.io/v1/exchangerate/BTC/{convertCurrency}?apikey={apikey}
     
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"];
+
+    func getCoinPrice(for currency: String)
+    {
+
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+
+        if let url = URL(string: urlString)
+        {
+
+            let session = URLSession(configuration: .default)
+
+            let task = session.dataTask(with: url)
+            {
+                (data, response, error) in
+                if error != nil
+                {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+
+                if let safeData = data
+                {
+                    if let bitcoinPrice = self.parseJSON(safeData)
+                    {
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+
+    func parseJSON(_ data: Data) -> Double?
+    {
+        let decoder = JSONDecoder()
+
+        do
+        {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            return lastPrice
+        }
+        catch
+        {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
 }
